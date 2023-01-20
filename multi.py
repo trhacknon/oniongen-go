ba5d69d8ef511fa59492cf8e18af522b99c26edf
trhacknon <98242014+trhacknon@users.noreply.github.com>
2023-01-20 20:45:45 +0100

    Update multi.py

--- multi.py
+++ multi.py
@@ -1,38 +1,30 @@
-import subprocess
-import threading
 import os
+from threading import Thread
 
-def generate_onion_link(prefix):
-    process = subprocess.Popen(["oniongen", "^"+prefix, "5"], stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    if error:
-        print(f"Erreur lors de la génération du lien onion : {error}")
+def generate_onion_address(prefix, number_of_links):
+    os.system(f"oniongen ^{prefix} {number_of_links}")
+
+def copy_keys():
+    copy_keys = input("Voulez-vous sauvegarder /var/lib/tor/hidden_service et copier les clés dans /var/lib/tor/hidden_service? (y/n)")
+    if copy_keys == 'y':
+        os.system("python3 cpkey.py")
     else:
-        links = output.strip().decode().split('\n')
-        for link in links:
-            print(f"Lien onion généré : {link}")
-            copy_key = input(f"Voulez-vous copier la clé privée dans /var/lib/tor/hidden_service/ ? (y/n)")
-            if copy_key == 'y':
-                key_path = "/var/lib/tor/hidden_service/"+link.replace(".onion","")
-                if os.path.exists(key_path):
-                    os.system(f"cp {key_path}/hs_ed25519_secret_key {key_path}/hs_ed25519_public_key {key_path}/hostname /var/lib/tor/hidden_service/")
-                    os.system("chown -R debian-tor /var/lib/tor")
-                    os.system("chmod 700 /var/lib/tor/hidden_service")
-                    os.system("sudo /etc/init.d/tor restart")
-                    os.system("sudo -u debian-tor tor")
-                    print(f"Les clés privées ont été copiées dans {key_path}")
-                else:
-                    print(f"Impossible de trouver les clés privées dans {key_path}")
+        print("Aucune copie de clé effectuée.")
 
-prefixes = ["anonym", "trhack", "trhacknn", "trh4ckn0", "trhack"]
+prefixes = input("Entrez les préfixes à utiliser (séparés par des espaces) : ")
+number_of_links = input("Entrez le nombre de liens à générer pour chaque préfixe : ")
+number_of_threads = input("Entrez le nombre de threads à utiliser : ")
+
+prefixes = prefixes.split(" ")
+
 threads = []
-
-# lancer 5 threads pour générer des liens onion de vanité
 for prefix in prefixes:
-    thread = threading.Thread(target=generate_onion_link, args=(prefix,))
-    thread.start()
-    threads.append(thread)
+    for i in range(int(number_of_threads)):
+        t = Thread(target=generate_onion_address, args=(prefix, number_of_links))
+        t.start()
+        threads.append(t)
 
-# attendre la fin de tous les threads
-for thread in threads:
-    thread.join()
+for t in threads:
+    t.join()
+
+copy_keys()
